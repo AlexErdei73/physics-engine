278f8721d4ea05bc50c409e8e829e39a854ccd2b
Alex Erdei <mralexerdei@yahoo.co.uk>
2025-06-17 01:47:46 +0100

    Make a simple project work and save in local storage

--- create/index.js
+++ create/index.js
@@ -1,23 +1,23 @@
 const emptyWorld = {
-	name: "Empty World",
-	g: 0,
-	scale: 0.01,
-	dt: 1e-5,
-	t: 0,
-	animTime: 1 / 30,
-	width: 600,
-	height: 400,
-	isTimeVisible: true,
-	isGridVisible: true,
-	isForcesVisible: false,
-	isEnergyVisible: false,
-	points: [],
-	rods: [],
+  name: "Empty World",
+  g: 0,
+  scale: 0.01,
+  dt: 1e-5,
+  t: 0,
+  animTime: 1 / 30,
+  width: 600,
+  height: 400,
+  isTimeVisible: true,
+  isGridVisible: true,
+  isForcesVisible: false,
+  isEnergyVisible: false,
+  points: [],
+  rods: [],
 };
 
-const projects = JSON.parse(localStorage.getItem("projects"));
+const projects = JSON.parse(localStorage.getItem("projects")) || [];
 
-const initialState = projects && projects.length > 0 ? projects[0] : emptyWorld;
+const initialState = projects.length > 0 ? projects[0] : emptyWorld;
 
 const inpName = document.querySelector("#inp-name");
 const inpG = document.querySelector("#inp-g");
@@ -55,108 +55,114 @@ const btnNewRod = document.querySelector("#btn-new-rod");
 const btnDeleteRod = document.querySelector("#btn-delete-rod");
 
 function editPoint(i) {
-	const { points } = initialState;
-	if (i < 0 || i >= points.length) return;
-	const point = points[i];
-	inpX.value = point.x;
-	inpY.value = point.y;
-	inpPointSize.value = point.size;
-	chkboxFixed.checked = point.isFixed;
-	if (point.isFixed) {
-		delete point.vx;
-		delete point.vy;
-		delete point.m;
-		inpVx.disabled = true;
-		inpVy.disabled = true;
-		inpM.disabled = true;
-	} else {
-		inpVx.value = point.vx;
-		inpVy.value = point.vy;
-		inpM.value = point.m;
-		inpVx.disabled = false;
-		inpVy.disabled = false;
-		inpM.disabled = false;
-	}
+  const { points } = initialState;
+  if (i < 0 || i >= points.length) return;
+  const point = points[i];
+  inpX.value = point.x;
+  inpY.value = point.y;
+  inpPointSize.value = point.size;
+  chkboxFixed.checked = point.isFixed;
+  if (point.isFixed) {
+    delete point.vx;
+    delete point.vy;
+    delete point.m;
+    inpVx.disabled = true;
+    inpVy.disabled = true;
+    inpM.disabled = true;
+  } else {
+    inpVx.value = point.vx;
+    inpVy.value = point.vy;
+    inpM.value = point.m;
+    inpVx.disabled = false;
+    inpVy.disabled = false;
+    inpM.disabled = false;
+  }
 }
 
 function addPoint() {
-	const { points } = initialState;
+  const { points } = initialState;
 
-	points.push({
-		x: 0,
-		y: 0,
-		vx: 0,
-		vy: 0,
-		m: 1,
-		size: 0.2,
-		isFixed: false,
-	});
+  points.push({
+    x: 0,
+    y: 0,
+    vx: 0,
+    vy: 0,
+    m: 1,
+    size: 0.2,
+    isFixed: false,
+  });
 
-	inpPointIndex.min = 0;
-	inpPointIndex.max = points.length - 1;
-	inpPointIndex.value = points.length - 1;
-	editPoint(points.length - 1);
+  inpPointIndex.min = 0;
+  inpPointIndex.max = points.length - 1;
+  inpPointIndex.value = points.length - 1;
+  editPoint(points.length - 1);
 }
 
 function deletePoint(i) {
-	const { points } = initialState;
-	if (i < 0 || i >= points.length) return;
+  const { points } = initialState;
+  if (i < 0 || i >= points.length) return;
 
-	if (points.length > 1) points.splice(i, 1);
+  if (points.length > 1) points.splice(i, 1);
 
-	inpPointIndex.max = points.length - 1;
-	inpPointIndex.value = points.length - 1;
-	editPoint(points.length - 1);
+  inpPointIndex.max = points.length - 1;
+  inpPointIndex.value = points.length - 1;
+  editPoint(points.length - 1);
 }
 
 function changePoint(i) {
-	const { points } = initialState;
-	if (i < 0 || i >= points.length) return;
+  const { points } = initialState;
+  if (i < 0 || i >= points.length) return;
 
-	const point = points[i];
-	point.x = +inpX.value || 0;
-	point.y = +inpY.value || 0;
-	point.size = +inpPointSize.value || 0.2;
-	point.isFixed = chkboxFixed.checked;
-	if (!point.isFixed) {
-		point.vx = +inpVx.value || 0;
-		point.vy = +inpVy.value || 0;
-		point.m = +inpM.value || 1;
-		inpVx.disabled = false;
-		inpVy.disabled = false;
-		inpM.disabled = false;
-	} else {
-		delete point.vx;
-		delete point.vy;
-		delete point.m;
-		inpVx.disabled = true;
-		inpVy.disabled = true;
-		inpM.disabled = true;
-	}
+  const point = points[i];
+  point.x = +inpX.value || 0;
+  point.y = +inpY.value || 0;
+  point.size = +inpPointSize.value || 0.2;
+  point.isFixed = chkboxFixed.checked;
+  if (!point.isFixed) {
+    point.vx = +inpVx.value || 0;
+    point.vy = +inpVy.value || 0;
+    point.m = +inpM.value || 1;
+    inpVx.disabled = false;
+    inpVy.disabled = false;
+    inpM.disabled = false;
+  } else {
+    delete point.vx;
+    delete point.vy;
+    delete point.m;
+    inpVx.disabled = true;
+    inpVy.disabled = true;
+    inpM.disabled = true;
+  }
 }
 
 function editParams() {
-	const {
-		name,
-		g,
-		dt,
-		animTime,
-		scale,
-		isTimeVisible,
-		isGridVisible,
-		isForcesVisible,
-		isEnergyVisible,
-	} = initialState;
+  const {
+    name,
+    g,
+    dt,
+    animTime,
+    scale,
+    isTimeVisible,
+    isGridVisible,
+    isForcesVisible,
+    isEnergyVisible,
+  } = initialState;
 
-	inpName.value = name;
-	inpG.value = g;
-	inpDt.value = dt;
-	inpAnimTime.value = animTime;
-	inpScale.value = scale;
-	chkboxShowTime.checked = isTimeVisible;
-	chkboxShowGrid.checked = isGridVisible;
-	chkboxShowForce.checked = isForcesVisible;
-	chkboxShowEnergy.checked = isEnergyVisible;
+  inpName.value = name;
+  inpG.value = g;
+  inpDt.value = dt;
+  inpAnimTime.value = animTime;
+  inpScale.value = scale;
+  chkboxShowTime.checked = isTimeVisible;
+  chkboxShowGrid.checked = isGridVisible;
+  chkboxShowForce.checked = isForcesVisible;
+  chkboxShowEnergy.checked = isEnergyVisible;
+}
+
+function save() {
+  initialState.name = inpName.value || initialState.name;
+  projects.push(initialState);
+  localStorage.setItem("projects", JSON.stringify(projects));
 }
 
 inpPointIndex.addEventListener("change", () => editPoint(inpPointIndex.value));
@@ -169,9 +175,17 @@ inpM.addEventListener("change", () => changePoint(inpPointIndex.value));
 inpPointSize.addEventListener("change", () => changePoint(inpPointIndex.value));
 btnNewPoint.addEventListener("click", addPoint);
 btnDeletePoint.addEventListener("click", () =>
-	deletePoint(inpPointIndex.value)
+  deletePoint(inpPointIndex.value)
 );
 
 const frm = document.querySelector("form");
 frm.addEventListener("submit", (event) => event.preventDefault());
 editParams();
+btnSave.addEventListener("click", save);
+const pointsLength = initialState.points.length;
+editPoint(pointsLength - 1);
+if (pointsLength > 0) {
+  inpPointIndex.min = 0;
+  inpPointIndex.max = pointsLength - 1;
+  inpPointIndex.value = pointsLength - 1;
+}
